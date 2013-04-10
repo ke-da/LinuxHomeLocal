71454078d561a9e4080b81fb55f8dad59e6252bd
Da Ke <keda@outlook.com>
2013-04-09 22:52:48 -0400

    add gitconfig

--- config/link.sh
+++ config/link.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 cd $HOME
 ln -s local/config/vim/vimrc .vimrc
+ln -s local/config/tmux-config/.tmux.conf .tmux.conf
+ln -s local/config/gitconfig .gitconfig
+
 mkdir -p .vim
 cd .vim
 ln -s ../local/config/vim/pathogen/autoload .
 ln -s ../local/config/vim/bundle .
+
